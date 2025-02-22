f186b74ea45880051edc7eb23ff4c78d4573b480
Bernardo Correa <bernardocorrea67@hotmail.com>
2025-02-22 16:20:25 -0300

    Resolução sem tratamento de exceções

--- src/app/Program.java
+++ src/app/Program.java
@@ -1,8 +1,34 @@
 package app;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+
 public class Program {
     public static void main(String[] args) {
-        
-        System.out.println("Hello, world!");
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Holder: ");
+        String holder = sc.nextLine();
+        System.out.print("Initial balance: ");
+        double initialBalance = sc.nextDouble();
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account acc = new Account(number, holder, initialBalance, withdrawLimit);
+
+        System.out.print("Enter amount for withdraw? ");
+        double amount = sc.nextDouble();
+
+        acc.withdraw(amount);
+        System.out.print("New balance: " + acc.getBalance());
+
+        sc.close();
     }
 }
